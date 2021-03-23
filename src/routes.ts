7737292abf973cbb6b334f4073f5912edd15200a
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-03-23 08:32:58 -0300

    Creating user exists route

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,24 @@
 //	Importing express resources
 import Router, { Request, Response } from "express";
 
+//	Importing route controllers
+import UsersRepository from "./repositories/UsersRepository";
+
 //  Setting up routes
 const routes = Router();
 
 //	Home page
 routes.get("/", (req: Request, res: Response) => {
 	return res.status(200).send("Chatzap Backend");
+});
+
+//	Return if user exists
+routes.get("/user/:number", (req: Request, res: Response) => {
+	const { number } = req.params;
+
+	const user = UsersRepository.findByNumber(number);
+
+	return res.status(200).json({ exists: (user) ? true : false });
 });
 
 //	Not found page
