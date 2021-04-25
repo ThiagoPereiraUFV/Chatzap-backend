da78954f51e4ee113624235a8e93fac6c2d95893
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-04-25 20:23:35 -0300

    Fixing internal server error

--- src/models/User.ts
+++ src/models/User.ts
@@ -24,7 +24,6 @@ const UserSchema = new Schema<User>({
 	password: {
 		type: String,
 		set: (p: String) => bcrypt.hashSync(p, bcrypt.genSaltSync(10)),
-		select: false,
 		required: true
 	},
 	online: {
