3d4e492df3cfab154d92a00d557a7154783834c1
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-06-10 14:41:28 -0300

    Fixing file delete bug

--- src/helpers/deleteFile.ts
+++ src/helpers/deleteFile.ts
@@ -2,7 +2,9 @@ import fs from "fs";
 
 export function deleteFile(filepath: string) {
 	try {
-		fs.unlinkSync(filepath);
+		if(fs.existsSync(filepath)) {
+			fs.unlinkSync(filepath);
+		}
 	} catch(error) {
 		throw new Error(error);
 	}
